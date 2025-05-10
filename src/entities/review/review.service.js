6bbba871e45b36b582623e0ffd5073bff2c25e26
NavidulHoque <hnavidul@gmail.com>
2025-05-10 15:13:14 +0600

    Fix rating validation in createReviewService and update resource retrieval to exclude unnecessary fields

--- src/entities/review/review.service.js
+++ src/entities/review/review.service.js
@@ -3,7 +3,7 @@ import Review from "./review.model.js";
 
 export const createReviewService = async ({ resourceId, userId, rating, comment }) => {
     if (!resourceId || !userId || !rating || !comment) throw new Error("All fields are required");
-    if (rating < 10 || rating > 5) throw new Error("Rating must be between 0 and 5");
+    if (rating < 0 || rating > 5) throw new Error("Rating must be between 0 and 5");
 
     const review = new Review({ resourceId, userId, rating, comment });
     await review.save();
