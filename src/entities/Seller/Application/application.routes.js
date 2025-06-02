654c6e1da598c6b69acfc9cdacd0ad24f3f86e7d
jarifMohammed <mohammedjarif@protonmail.com>
2025-06-03 05:52:51 +0600

    Fix formatting in application routes file by adding missing newline at end of file

--- src/entities/Seller/Application/application.routes.js
+++ src/entities/Seller/Application/application.routes.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import { applyToBecomeSellerController, getAllSellerApplicationsController, updateSellerApplicationStatusController } from './application.controller';
 import { adminMiddleware, verifyToken } from '../../../core/middlewares/authMiddleware';
-
-
-
-
 const router = express.Router();
 
 router.post('/apply',applyToBecomeSellerController)
 router.get('/admin/applications',verifyToken,adminMiddleware,getAllSellerApplicationsController)
 router.patch('/admin/applications/:id',verifyToken,adminMiddleware,updateSellerApplicationStatusController)
+
+export default router;
