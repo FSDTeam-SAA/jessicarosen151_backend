c9543afeac7f143780809fa67ef1cd67ba75f533
jarifMohammed <mohammedjarif@protonmail.com>
2025-06-03 05:52:15 +0600

    Add authentication middleware to update seller application status route

--- src/entities/Seller/Application/application.routes.js
+++ src/entities/Seller/Application/application.routes.js
@@ -9,4 +9,4 @@ const router = express.Router();
 
 router.post('/apply',applyToBecomeSellerController)
 router.get('/admin/applications',verifyToken,adminMiddleware,getAllSellerApplicationsController)
-router.patch('/admin/applications/:id',updateSellerApplicationStatusController)
+router.patch('/admin/applications/:id',verifyToken,adminMiddleware,updateSellerApplicationStatusController)
