d5fdd27e5fc23d8dc68a738704945d6204f7ecd0
jarifMohammed <mohammedjarif@protonmail.com>
2025-06-03 05:51:58 +0600

    Add authentication middleware to admin applications route

--- src/entities/Seller/Application/application.routes.js
+++ src/entities/Seller/Application/application.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { applyToBecomeSellerController, getAllSellerApplicationsController, updateSellerApplicationStatusController } from './application.controller';
+import { adminMiddleware, verifyToken } from '../../../core/middlewares/authMiddleware';
 
 
 
@@ -7,5 +8,5 @@ import { applyToBecomeSellerController, getAllSellerApplicationsController, upda
 const router = express.Router();
 
 router.post('/apply',applyToBecomeSellerController)
-router.get('/admin/applications',getAllSellerApplicationsController)
+router.get('/admin/applications',verifyToken,adminMiddleware,getAllSellerApplicationsController)
 router.patch('/admin/applications/:id',updateSellerApplicationStatusController)
