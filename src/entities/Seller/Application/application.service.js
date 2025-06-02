f18ecf2b25fcf3afbd23136cf7251cd66da61f6f
jarifMohammed <mohammedjarif@protonmail.com>
2025-06-03 05:35:21 +0600

    Implement createApplication service to handle user registration with email validation and default seller status

---
+++ src/entities/Seller/Application/application.service.js
@@ -0,0 +1,26 @@
+import RoleType from "../../../lib/types";
+import User from "../../auth/auth.model";
+
+export const createApplication = async (data) => {
+
+    const existingEmail = await User.findOne({ email: data.email })
+    if (existingEmail) {
+        throw new Error('Email already exists');
+    }
+
+    // create new application 
+
+    const user = new User.create({
+        ...data,
+        role: RoleType.USER,
+        sellerStatus: 'pending'
+    })
+
+    const userObj = user.toObject();
+    delete userObj.password;
+    return userObj;
+
+}
+
+
+
