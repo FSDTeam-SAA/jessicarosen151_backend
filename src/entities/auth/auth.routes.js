9172b980c919445b90e7c63b5ad1ce50e325b962
jishad10 <jishadjishad49@gmail.com>
2025-06-04 12:10:40 +0600

    Refactor auth middleware and routes: reorganize logoutUser function and update routes for improved structure and clarity

--- src/entities/auth/auth.routes.js
+++ src/entities/auth/auth.routes.js
@@ -6,11 +6,14 @@ import {
   forgetPassword,
   verifyCode,
   resetPassword,
-  changePassword
+  changePassword,
+  logoutUser
 } from './auth.controller.js';
-import { verifyToken } from '../../core/middlewares/authMiddleware.js';
+import { userAdminSellerMiddleware, verifyToken } from '../../core/middlewares/authMiddleware.js';
+
 
 const router = express.Router();
+
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
@@ -19,5 +22,7 @@ router.post('/forget-password', forgetPassword);
 router.post('/verify-code', verifyCode);
 router.post('/reset-password', resetPassword);
 router.post('/change-password',verifyToken, changePassword);
+router.post('/logout', verifyToken, userAdminSellerMiddleware, logoutUser);
+
 
 export default router;
