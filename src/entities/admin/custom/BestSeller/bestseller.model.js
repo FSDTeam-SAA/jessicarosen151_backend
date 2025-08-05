87fe8afef8f112140df36be374556415ac4ee3a8
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-08-05 10:17:15 +0600

    best seller

--- src/entities/admin/custom/BestSeller/bestseller.model.js
+++ src/entities/admin/custom/BestSeller/bestseller.model.js
@@ -13,16 +13,7 @@ const bestsellerSchema = new Schema(
     image: [{
       type: String,
       required: true,
-      validate: {
-        validator: (v) => v.length <= 2,
-        message: "image array can have maximum length of 2",
-      },
     }],
-    
-    country: {
-      type: String,
-      required: true,
-    },
   },
   {
     timestamps: true,
