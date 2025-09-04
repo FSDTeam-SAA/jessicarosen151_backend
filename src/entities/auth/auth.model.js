77243b4b704b7030f918338bbab85a74dfff4298
MD MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-09-04 10:33:13 +0600

    Add 'about' field to User schema with default value

--- src/entities/auth/auth.model.js
+++ src/entities/auth/auth.model.js
@@ -20,6 +20,7 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     dob: { type: Date, default: null },
+    about: { type: String, default: '' },
     gender: {
       type: String,
       enum: ['male', 'female', 'other'],
